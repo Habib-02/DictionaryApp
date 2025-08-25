37549ac3dd1b71bd1725b25e9b5f93987a32ca84
habib <moh.habibur.7@gmail.com>
2025-08-25 16:21:04 +0300

    fix the issue with the mobile keyboard

--- src/components/Dictionary/SearchInput/SearchInput.jsx
+++ src/components/Dictionary/SearchInput/SearchInput.jsx
@@ -6,6 +6,8 @@ import { Search } from "lucide-react";
 import styles from "./SearchInput.module.css";
 
 function SearchInput({ onSubmit }) {
+  const inputRef = React.useRef(null);
+
   return (
     <>
       <Formik
@@ -22,12 +24,18 @@ function SearchInput({ onSubmit }) {
         onSubmit={(values, { resetForm }) => {
           onSubmit(values.search.toLocaleLowerCase());
           resetForm();
+
+          // blur the input to hide the mobile keyboard
+          if (inputRef.current) {
+            inputRef.current.blur();
+          }
         }}
       >
         {({ errors }) => (
           <Form>
             <div className={styles.inputGroup}>
               <Field
+                innerRef={inputRef}
                 name="search"
                 type="text"
                 placeholder="Search for any word"
